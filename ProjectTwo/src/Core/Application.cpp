2bf2006b1ff9a2d444b4d4edf22b01c6c22a8e5f
Rich <rfspencer@users.noreply.github.com>
2024-07-21 11:52:13 -0700

    Adds basic drawing to console buffer

--- ProjectTwo/src/Core/Application.cpp
+++ ProjectTwo/src/Core/Application.cpp
@@ -3,19 +3,61 @@
 #include <iostream>
 #include <ostream>
 
-Application::Application()
+Application::Application(const short InWindowWidth, const short InWindowHeight, const std::wstring& InTitle)
+    : m_WindowWidth(InWindowWidth), m_WindowHeight(InWindowHeight), m_Title(InTitle), m_bRenderIsDirty(true)
 {
-    std::cout << "Application Constructed" << std::endl;
-}
-
-Application::~Application()
-{
-    std::cout << "Application Destroyed" << std::endl;
 }
 
 void Application::Run()
 {
     std::cout << "Application Running" << std::endl;
+
+    bool bAppRunning = true;
+    while (bAppRunning)
+    {
+        // Poll input events
+        
+        // Dispatch events
+        
+        // Handle tick, timers
+
+        // Render
+        if (m_bRenderIsDirty)
+        {
+            RenderInternal();
+        }
+    }
 }
 
+void Application::RenderInternal()
+{
+    // Clear Screen buffer
 
+    // Render()
+
+    // Display buffer
+
+    
+    m_bRenderIsDirty = false;
+    
+    // Get the console handle
+    m_ConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    // Set the console window size
+    m_ConsoleBufferSize = {m_WindowWidth, m_WindowHeight};
+    SetConsoleScreenBufferSize(m_ConsoleHandle, m_ConsoleBufferSize);
+    // Get the console screen buffer info
+    CONSOLE_SCREEN_BUFFER_INFO CSBI;
+    GetConsoleScreenBufferInfo(m_ConsoleHandle, &CSBI);
+
+    // Calculate the position to center the text, screen width and height from CSBI
+    COORD CursorPosition;
+    CursorPosition.X = (CSBI.dwSize.X - m_Title.length()) / 2;
+    CursorPosition.Y = CSBI.dwSize.Y / 2;
+
+    // Set cursor position
+    SetConsoleCursorPosition(m_ConsoleHandle, CursorPosition);
+
+    // Write the text to the console
+    DWORD Length;
+    WriteConsoleW(m_ConsoleHandle, m_Title.c_str(), m_Title.length(), nullptr, nullptr);
+}
--- ProjectTwo/src/Core/Application.h
+++ ProjectTwo/src/Core/Application.h
@@ -1,15 +1,30 @@
 ﻿#pragma once
+#include <string>
+
+#include "Windows.h"
 
 class Application
 {
 public:
-    Application();
-    ~Application();
-    
+    Application() = default;
+    Application(short InWindowWidth, short InWindowHeight, const std::wstring& InTitle);
+
     void Run();
 
-private:
+    void SetRenderIsDirty(const bool bRenderIsDirty) { m_bRenderIsDirty = bRenderIsDirty; }
 
+private:
+    void RenderInternal();
+    
+private:
+    HANDLE m_ConsoleHandle;
+    COORD m_ConsoleBufferSize;
+
+    short m_WindowWidth;
+    short m_WindowHeight;
+    std::wstring m_Title;
+
+    bool m_bRenderIsDirty;
 };
 
 // Gets defined in the Game
