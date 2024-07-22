18411282bbacc599e39b5e53a9144d8e39c75bbb
Rich <rfspencer@users.noreply.github.com>
2024-07-21 17:25:00 -0700

    Adds Types header

---
+++ ProjectTwo/src/Core/Types.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <memory>
+#include <vector>
+
+template<typename T>
+using UniquePtr = std::unique_ptr<T>;
+
+template<typename T>
+using SharedPtr = std::shared_ptr<T>;
+
+template<typename T>
+using WeakPtr = std::weak_ptr<T>;
+
+template<typename T>
+using List = std::vector<T>;
