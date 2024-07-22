95801d5647ec21a076ba4ddfae96040552b97e29
Rich <rfspencer@users.noreply.github.com>
2024-07-21 17:27:44 -0700

    Adds World class with basic implementation

---
+++ ProjectTwo/src/Core/World.cpp
@@ -0,0 +1,30 @@
+﻿#include "World.h"
+
+
+World::World(Application* OwningApp)
+    : m_OwningApp(OwningApp), m_bBeginPlay(false)
+{
+}
+
+void World::BeginPlayInternal()
+{
+    if (!m_bBeginPlay)
+    {
+        m_bBeginPlay = true;
+        BeginPlay();
+        
+    }
+}
+
+void World::TickInternal(float DeltaTime)
+{
+    Tick(DeltaTime);
+}
+
+void World::BeginPlay()
+{
+}
+
+void World::Tick(float DeltaTime)
+{
+}
---
+++ ProjectTwo/src/Core/World.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "Object.h"
+#include "Types.h"
+
+class Application;
+
+class World : public Object
+{
+public:
+    World(Application* OwningApp);
+    
+    virtual void BeginPlayInternal();
+    virtual void TickInternal(float DeltaTime);
+
+    Application* GetApplication() { return m_OwningApp; }
+    const Application* GetApplication() const { return m_OwningApp; }
+
+private:
+    virtual void BeginPlay();
+    virtual void Tick(float DeltaTime);
+    
+private:
+    Application* m_OwningApp;
+    bool m_bBeginPlay;
+    
+};
