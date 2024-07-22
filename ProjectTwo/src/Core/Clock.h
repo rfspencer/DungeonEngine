f4231061d53c7f25c359af8206b86574e73914a9
Rich <rfspencer@users.noreply.github.com>
2024-07-21 17:23:33 -0700

    Adds Clock class to calculate DeltaTime

---
+++ ProjectTwo/src/Core/Clock.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include <chrono>
+
+class Clock
+{
+public:
+    Clock()
+    {
+        m_Time = Clock_T::now();
+    }
+
+    float GetElapsed() const
+    {
+        const std::chrono::duration<float> Elapsed = Clock_T::now() - m_Time;
+        return Elapsed.count();
+    }
+    
+    float Restart()
+    {
+        const float Elapsed = GetElapsed();
+        m_Time = Clock_T::now();
+        return Elapsed;
+    }
+    
+private:
+    using Clock_T = std::chrono::high_resolution_clock;
+    using Time_T = std::chrono::time_point<Clock_T>;
+    Time_T m_Time;
+    
+};
