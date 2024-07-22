aa49b3a2561cd338262d52c08137a9db379c48ba
Rich <rfspencer@users.noreply.github.com>
2024-07-21 17:28:12 -0700

    Adds derived MainMenuLevel

---
+++ ProjectTwo/src/Game/MainMenuLevel.cpp
@@ -0,0 +1,26 @@
+﻿#include "MainMenuLevel.h"
+#include <iostream>
+
+MainMenuLevel::MainMenuLevel(Application* OwningApp)
+    : World(OwningApp)
+{
+}
+
+void MainMenuLevel::BeginPlay()
+{
+    std::cout << "Dungeon Crawler" << std::endl << std:: endl;
+    std::cout << "Main Menu" << std::endl;
+}
+
+void MainMenuLevel::Tick(float DeltaTime)
+{
+    // Poll input for menu selection
+}
+
+void MainMenuLevel::StartGame()
+{
+}
+
+void MainMenuLevel::QuitGame()
+{
+}
---
+++ ProjectTwo/src/Game/MainMenuLevel.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "World.h"
+
+class MainMenuLevel : public World
+{
+public:
+    MainMenuLevel(Application* OwningApp);
+    void BeginPlay() override;
+    void Tick(float DeltaTime) override;
+
+private:
+    void StartGame();
+    void QuitGame();
+    
+};
