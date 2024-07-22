6b40c2c07181cbc0357911ee601349b3871891ba
Rich <rfspencer@users.noreply.github.com>
2024-07-21 17:26:45 -0700

    Adds base Object class for all game objects to derive from

---
+++ ProjectTwo/src/Core/Object.cpp
@@ -0,0 +1,16 @@
+﻿#include "Object.h"
+
+unsigned int Object::UniqueIDCounter = 0;
+Object::Object()
+    : m_UniqueID(GetNextAvailableID())
+{
+}
+
+Object::~Object()
+{
+}
+
+unsigned int Object::GetNextAvailableID()
+{
+    return UniqueIDCounter++;
+}
---
+++ ProjectTwo/src/Core/Object.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <memory>
+
+/**
+ * Object class for all game objects to derive from.
+ * @ref <CPP> https://en.cppreference.com/w/cpp/memory/enable_shared_from_this
+ */
+class Object : public std::enable_shared_from_this<Object>
+{
+public:
+    Object();
+    virtual ~Object();
+
+    unsigned int GetUniqueID() const { return m_UniqueID; }
+
+private:
+    // Game wide, every object has a unique identifier
+    unsigned int m_UniqueID;
+
+    static unsigned int UniqueIDCounter;
+    // Increments UniqueIDCounter and returns the next Object ID in sequence
+    static unsigned int GetNextAvailableID();
+};
