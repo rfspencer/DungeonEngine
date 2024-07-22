777297a345d7d4b671bb194183154179217eb7ce
Rich <rfspencer@users.noreply.github.com>
2024-07-21 17:29:24 -0700

    Loads MainMenuLevel at start up

--- ProjectTwo/src/Game/DungeonGame.cpp
+++ ProjectTwo/src/Game/DungeonGame.cpp
@@ -4,24 +4,25 @@
 
 #include <Core.h>
 
+#include "MainMenuLevel.h"
+
 const std::wstring GAME_NAME = L"Dungeon Crawler";
 constexpr short WINDOW_WIDTH = 90;
 constexpr short WINDOW_HEIGHT = 15;
 
-class Game : public Application
+class DungeonGame : public Application
 {
 public:
-    Game(short InWindowWidth, short InWindowHeight, const std::wstring& InTitle)
+    DungeonGame(short InWindowWidth, short InWindowHeight, const std::wstring& InTitle)
         : Application(InWindowWidth, InWindowHeight, InTitle)
     {
         // Load Asset Manager
         // Load Main Menu World
-        
-        std::cout << "Game Constructed" << std::endl;
+        WeakPtr<MainMenuLevel> NewWorld = LoadWorld<MainMenuLevel>();
     }
 };
 
 Application* GetApplication()
 {
-    return new Game(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_NAME);
+    return new DungeonGame(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_NAME);
 }
