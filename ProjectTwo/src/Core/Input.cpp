63dd3b44926dfe016be9424c4a7fb932d5af7083
Rich <rfspencer@users.noreply.github.com>
2024-07-24 22:40:03 -0700

    Base implementation of Input class

---
+++ ProjectTwo/src/Core/Input.cpp
@@ -0,0 +1,41 @@
+﻿#include "Input.h"
+
+#include <conio.h>
+
+void Input::Update()
+{
+    if (_kbhit())
+    {
+        m_KeyDown = _getch();
+
+        for (auto& Listener : m_InputListeners)
+        {
+            Listener(m_KeyDown);
+        }
+    }
+    else
+    {
+        m_KeyDown = 0;
+    }
+    
+}
+
+int Input::GetKeyDown()
+{
+    return m_KeyDown;
+}
+
+void Input::AddListener(std::function<void(int Input)> Callback)
+{
+    m_InputListeners.push_back(Callback);
+}
+
+void Input::RemoveListener(std::function<void(int Input)> Callback)
+{
+    auto it = std::remove_if(m_InputListeners.begin(), m_InputListeners.end(), [Callback](const auto& Listener)
+    {
+       return Listener.target_type() == Callback.target_type(); 
+    });
+    m_InputListeners.erase(it, m_InputListeners.end());
+}
+
---
+++ ProjectTwo/src/Core/Input.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <functional>
+#include <vector>
+
+class Input
+{
+public:
+    Input() = default;
+
+    static void Update();
+    static int GetKeyDown();
+    static void AddListener(std::function<void(int Input)> Callback);
+    static void RemoveListener(std::function<void(int Input)> Callback);
+    static void CleanUp() { m_InputListeners.clear(); }
+
+private:
+    static int m_KeyDown;
+    static std::vector<std::function<void(int)>> m_InputListeners;
+};
