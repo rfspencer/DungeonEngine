488326e725af6a9f5b1f08b6cb2934a3c162be54
Rich <rfspencer@users.noreply.github.com>
2024-07-25 20:58:44 -0700

    Adds HUD base class

---
+++ ProjectTwo/src/Core/Widgets/HUD.cpp
@@ -0,0 +1,28 @@
+﻿#include "Widgets/HUD.h"
+
+HUD::HUD()
+    : m_HasInit(false)
+{
+}
+
+void HUD::InitInternal()
+{
+    if (!m_HasInit)
+    {
+        m_HasInit = true;
+        Init();
+    }
+}
+
+void HUD::Init()
+{
+}
+
+bool HUD::HandleEvent()
+{
+    return false;
+}
+
+void HUD::Tick(float DeltaTime)
+{
+}
---
+++ ProjectTwo/src/Core/Widgets/HUD.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "Core/Object.h"
+
+class Renderer;
+
+class HUD : public Object
+{
+public:
+    virtual void Render(Renderer& InRendererRef) = 0;
+    void InitInternal();
+
+    bool HasInit() const { return m_HasInit; }
+
+    virtual bool HandleEvent();
+
+    virtual void Tick(float DeltaTime);
+
+protected:
+    HUD();
+
+private:
+    virtual void Init();
+
+    bool m_HasInit;
+};
