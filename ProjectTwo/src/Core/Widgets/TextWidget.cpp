90ceb236b2e96f82a4770ab75c6420a093e25374
Rich <rfspencer@users.noreply.github.com>
2024-07-25 20:56:40 -0700

    Adds TextWidget class

---
+++ ProjectTwo/src/Core/Widgets/TextWidget.cpp
@@ -0,0 +1,22 @@
+﻿#include "TextWidget.h"
+
+#include <iostream>
+
+#include "Renderer.h"
+
+TextWidget::TextWidget(const std::string& InText)
+    : m_Text(InText)
+{
+}
+
+void TextWidget::SetText(const std::string& InString)
+{
+    m_Text = InString;
+    SetDoesNeedUpdate(true);
+}
+
+void TextWidget::Render(Renderer& InRendererRef)
+{
+    InRendererRef.DrawUI(*this, GetWidgetPosition());
+    SetDoesNeedUpdate(false);
+}
---
+++ ProjectTwo/src/Core/Widgets/TextWidget.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <string>
+
+#include "Widget.h"
+
+class TextWidget : public Widget
+{
+public:
+    TextWidget() = default;
+    TextWidget(const std::string& InText);
+
+    void SetText(const std::string& InString);
+    std::string GetText() { return m_Text; }
+
+private:
+    void Render(Renderer& InRendererRef) override;
+
+    std::string m_Text;
+};
