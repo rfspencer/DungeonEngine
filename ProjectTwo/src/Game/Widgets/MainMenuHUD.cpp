c8e0592ac9f3eaebc78a249dd220c423deeee4a9
Rich <rfspencer@users.noreply.github.com>
2024-07-25 20:59:07 -0700

    Adds MainMenuHUD class

---
+++ ProjectTwo/src/Game/Widgets/MainMenuHUD.cpp
@@ -0,0 +1,32 @@
+﻿#include "MainMenuHUD.h"
+
+#include <iostream>
+#include <ostream>
+
+MainMenuHUD::MainMenuHUD()
+    : m_MenuTitleText("Main Menu"),
+      m_NewGameText("New Game"),
+      m_QuitGameText("Quit")
+{
+    Init();
+}
+
+void MainMenuHUD::Render(Renderer& InRendererRef)
+{
+    m_MenuTitleText.RenderInternal(InRendererRef);
+    m_NewGameText.RenderInternal(InRendererRef);
+    m_QuitGameText.RenderInternal(InRendererRef);
+}
+
+bool MainMenuHUD::HandleEvent()
+{
+    return false;
+}
+
+void MainMenuHUD::Init()
+{
+    m_MainMenuLayout.SetWidgetPosition(0, 0);
+    m_MenuTitleText.SetWidgetPosition(40, 10);
+    m_NewGameText.SetWidgetPosition(40, 11);
+    m_QuitGameText.SetWidgetPosition(40, 12);
+}
---
+++ ProjectTwo/src/Game/Widgets/MainMenuHUD.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "Widgets/HUD.h"
+#include "Widgets/TextWidget.h"
+
+class MainMenuHUD : public HUD
+{
+public:
+    MainMenuHUD();
+
+    void Render(Renderer& InRendererRef) override;
+    bool HandleEvent() override;
+
+private:
+    void Init() override;
+
+    TextWidget m_MainMenuLayout;
+    TextWidget m_MenuTitleText;
+    TextWidget m_NewGameText;
+    TextWidget m_QuitGameText;
+
+};
