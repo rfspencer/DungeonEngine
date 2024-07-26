52b603a5a50c5bd23fb05135ffba2e5d7c696ad5
Rich <rfspencer@users.noreply.github.com>
2024-07-25 21:06:02 -0700

    Adds helper functions to Object

--- ProjectTwo/src/Core/Object.cpp
+++ ProjectTwo/src/Core/Object.cpp
@@ -2,12 +2,27 @@
 
 unsigned int Object::UniqueIDCounter = 0;
 Object::Object()
-    : m_UniqueID(GetNextAvailableID())
+    : m_UniqueID(GetNextAvailableID()), m_IsPendingDestroy(false)
 {
 }
 
 Object::~Object()
 {
+}
+
+void Object::Destroy()
+{
+    m_IsPendingDestroy = true;
+}
+
+WeakPtr<Object> Object::GetWeakRef()
+{
+    return weak_from_this();
+}
+
+WeakPtr<const Object> Object::GetWeakRef() const
+{
+    return weak_from_this();
 }
 
 unsigned int Object::GetNextAvailableID()
--- ProjectTwo/src/Core/Object.h
+++ ProjectTwo/src/Core/Object.h
@@ -2,6 +2,8 @@
 
 #include <memory>
 
+#include "Types.h"
+
 /**
  * Object class for all game objects to derive from.
  * @ref <CPP> https://en.cppreference.com/w/cpp/memory/enable_shared_from_this
@@ -12,11 +14,19 @@ public:
     Object();
     virtual ~Object();
 
+    virtual void Destroy();
+    bool IsPendingDestroy() const { return m_IsPendingDestroy; }
+
+    WeakPtr<Object> GetWeakRef();
+    WeakPtr<const Object> GetWeakRef() const;
+
     unsigned int GetUniqueID() const { return m_UniqueID; }
 
 private:
     // Game wide, every object has a unique identifier
     unsigned int m_UniqueID;
+
+    bool m_IsPendingDestroy;
 
     static unsigned int UniqueIDCounter;
     // Increments UniqueIDCounter and returns the next Object ID in sequence
