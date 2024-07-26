6e37bde9a5b6f1510fb374e59a5340e88403968a
Rich <rfspencer@users.noreply.github.com>
2024-07-25 20:56:20 -0700

    Adds Widget base class

---
+++ ProjectTwo/src/Core/Widgets/Widget.cpp
@@ -0,0 +1,42 @@
+﻿#include "Widget.h"
+
+Widget::Widget()
+    : m_WidgetTransform(), m_IsVisible(true), m_DoesNeedUpdate(true)
+{
+}
+
+void Widget::RenderInternal(Renderer& InRendererRef)
+{
+    if(m_IsVisible && GetDoesNeedUpdate())
+    {
+        Render(InRendererRef);
+    }
+}
+
+void Widget::SetWidgetPosition(Vector2i InNewLocation)
+{
+    m_WidgetTransform.SetPosition(InNewLocation);
+    LocationUpdated(InNewLocation);
+    SetDoesNeedUpdate(true);
+}
+
+void Widget::SetWidgetPosition(int InX, int InY)
+{
+    m_WidgetTransform.SetPosition({InX, InY});
+    LocationUpdated({InX, InY});
+    SetDoesNeedUpdate(true);
+}
+
+void Widget::SetVisibility(bool InNewVisibility)
+{
+    m_IsVisible = InNewVisibility;
+    SetDoesNeedUpdate(true);
+}
+
+void Widget::Render(Renderer& InRendererRef)
+{
+}
+
+void Widget::LocationUpdated(const Vector2i InNewLocation)
+{
+}
---
+++ ProjectTwo/src/Core/Widgets/Widget.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "Object.h"
+
+#include "Utilities/Transform.h"
+
+class Renderer;
+
+class Widget : Object
+{
+public:
+    void RenderInternal(Renderer& InRendererRef);
+
+    void SetWidgetPosition(Vector2i InNewLocation);
+    void SetWidgetPosition(int InX, int InY);
+    Vector2i GetWidgetPosition() const { return m_WidgetTransform.GetPosition(); }
+
+    void SetVisibility(bool InNewVisibility);
+    bool GetVisibility() const { return m_IsVisible; }
+
+    void SetDoesNeedUpdate(bool InDoesNeedUpdate) { m_DoesNeedUpdate = InDoesNeedUpdate; }
+    bool GetDoesNeedUpdate() const { return m_DoesNeedUpdate; }
+
+protected:
+    Widget();
+    
+private:
+    virtual void Render(Renderer& InRendererRef);
+    virtual void LocationUpdated(const Vector2i InNewLocation);
+
+    Transform m_WidgetTransform;
+    bool m_IsVisible;
+    bool m_DoesNeedUpdate;
+};
