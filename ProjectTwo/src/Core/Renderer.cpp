4ecf4301cca7906e0de361695969604562955441
Rich <rfspencer@users.noreply.github.com>
2024-07-25 21:05:38 -0700

    Finishes HUD rendering

--- ProjectTwo/src/Core/Renderer.cpp
+++ ProjectTwo/src/Core/Renderer.cpp
@@ -1,1 +1,116 @@
-﻿#include "Renderer.h"
+﻿#include "Core/Renderer.h"
+
+#include <ostream>
+
+#include "Widgets/TextWidget.h"
+#include "Widgets/Widget.h"
+
+Renderer::Renderer()
+    : m_RenderBuffer()
+{
+    FixConsoleWindow();
+    HideCursor();
+}
+
+void Renderer::Init(std::wstring InTitle)
+{
+    // Initializing the buffer to remove garbage from previous console sessions
+    HANDLE OutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD DWBufferSize = { WINDOW_WIDTH, WINDOW_HEIGHT };
+    COORD DWBufferCoord = { 0, 0 };
+    SMALL_RECT RCRegion = {0, 0, WINDOW_WIDTH - 1, WINDOW_HEIGHT - 1 };
+
+    ReadConsoleOutput(OutputHandle, m_RenderBuffer.data(), DWBufferSize, DWBufferCoord, &RCRegion);
+}
+
+void Renderer::ClearConsoleScreen()
+{
+    HANDLE OutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO CSBI;
+    DWORD CellCount;
+    DWORD Count;
+    
+    // Get the number cells in the current buffer
+    if (!GetConsoleScreenBufferInfo(OutputHandle, &CSBI)) return;
+    CellCount = CSBI.dwSize.X *CSBI.dwSize.Y;
+
+    // Fill the entire buffer with spaces
+    if (!FillConsoleOutputCharacter(OutputHandle, (TCHAR)' ', CellCount, {0,0}, &Count)) return;
+    if (!FillConsoleOutputAttribute(OutputHandle, CSBI.wAttributes, CellCount, {0,0}, &Count)) return;
+    // Move cursor home
+    SetConsoleCursorPosition(OutputHandle, {0,0});
+}
+
+void Renderer::DisplayRenderBuffer()
+{
+    HANDLE OutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD DWBufferSize = {WINDOW_WIDTH, WINDOW_HEIGHT};
+    COORD DWBufferCoord = {0, 0};
+    SMALL_RECT RCRegion = {0, 0, WINDOW_WIDTH - 1, WINDOW_HEIGHT - 1 };
+
+    WriteConsoleOutput(OutputHandle, m_RenderBuffer.data(), DWBufferSize, DWBufferCoord, &RCRegion);
+}
+
+void Renderer::FixConsoleWindow()
+{
+    HWND ConsoleWindow = GetConsoleWindow();
+    LONG Style = GetWindowLong(ConsoleWindow, GWL_STYLE);
+    Style = Style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+    SetWindowLong(ConsoleWindow, GWL_STYLE, Style);
+}
+
+void Renderer::HideCursor()
+{
+    HANDLE ConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO CursorInfo;
+    CursorInfo.bVisible = 0;
+    CursorInfo.dwSize = 1;
+    SetConsoleCursorInfo(ConsoleOutput, &CursorInfo);
+}
+
+void Renderer::GoToXY(int InX, int InY)
+{
+    COORD Coord;
+    Coord.X = InX;
+    Coord.Y = InY;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Coord);
+}
+
+void Renderer::DrawObject(const Object& InObject)
+{
+    // Actor calls this to add itself to a location in the Render buffer
+}
+
+void Renderer::DrawUI(Widget& InWidget, Vector2i InPosition)
+{
+    // TODO: Need to setup a Delegate system for the UI so the UI can update when game state changes
+    // If we're trying to draw a TextWidget
+    if(TextWidget* Text = dynamic_cast<TextWidget*>(&InWidget))
+    {
+        size_t Length = Text->GetText().length();
+        for(size_t i = 0; i < Text->GetText().length(); ++i)
+        {
+            CHAR_INFO Sprite;
+            Sprite.Char.UnicodeChar = Text->GetText().at(i);
+            Sprite.Attributes = 0x0E; // Yellow color in decimal
+            Vector2i Position = Text->GetWidgetPosition();
+            Position.X += static_cast<int>(i);
+
+            AddElementToRenderBuffer(Sprite, Position);
+        }
+    }
+}
+
+void Renderer::AddElementToRenderBuffer(CHAR_INFO InSprite, Vector2i InPosition)
+{
+    // Y is the stride (rows), Y * 120. X is the count inside current stride (columns).
+    // Add the element to render to the buffer
+    int Y = InPosition.Y;
+    int X = InPosition.X;
+    m_RenderBuffer[InPosition.Y * 120 + InPosition.X] = InSprite;
+}
+
+void Renderer::SetConsoleColor(int InColor)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), InColor);
+}
--- ProjectTwo/src/Core/Renderer.h
+++ ProjectTwo/src/Core/Renderer.h
@@ -1,7 +1,38 @@
 ﻿#pragma once
 
+#include <array>
+#include <string>
+
+#include "Windows.h"
+#include "Core/World.h"
+#include "Game/Utilities/Constants.h" // TODO: not great to include game stuff here. Find a way to keep this in Game
+#include "Utilities/Vector2i.h"
+
+class Widget;
+
 class Renderer
 {
 public:
+    Renderer();
     
+    void Init(std::wstring InTitle);
+
+    void ClearConsoleScreen();
+
+    void DrawObject(const Object& InObject);
+    void DrawUI(Widget& InWidget, Vector2i InPosition);
+    
+    void DisplayRenderBuffer();
+    
+private:
+    void FixConsoleWindow();
+    void HideCursor();
+    void GoToXY(int InX, int InY);
+    void AddElementToRenderBuffer(CHAR_INFO InSprite, Vector2i InPosition);
+
+    void SetConsoleColor(int InColor);
+
+    // Render buffer array
+    std::array<CHAR_INFO, WINDOW_WIDTH * WINDOW_HEIGHT> m_RenderBuffer;
 };
+
