14589cc5cb352e00a117c9a077b888f97d835e6f
Rich <rfspencer@users.noreply.github.com>
2024-07-25 21:06:47 -0700

    Adds Actor class

---
+++ ProjectTwo/src/Core/Actor.cpp
@@ -0,0 +1,51 @@
+﻿#include "Core/Actor.h"
+
+#include "Core/Renderer.h"
+
+Actor::Actor(World* InOwningWorld)
+    : m_OwningWorld(InOwningWorld), m_HasBeganPlay(false), m_IsRenderable(true)
+{
+}
+
+Actor::~Actor()
+{
+}
+
+void Actor::BeginPlayInternal()
+{
+}
+
+void Actor::TickInternal(float DeltaTime)
+{
+}
+
+void Actor::BeginPlay()
+{
+}
+
+void Actor::Tick(float DeltaTime)
+{
+}
+
+void Actor::Render(Renderer& InRendererRef)
+{
+    if (IsPendingDestroy())
+    {
+        return;
+    }
+    //InRendererRef.DrawObject();
+}
+
+void Actor::SetActorLocation(const Vector2i InNewLocation)
+{
+    m_Transform.SetPosition(InNewLocation);
+}
+
+void Actor::Destroy()
+{
+    Object::Destroy();
+}
+
+void Actor::ApplyDamage(float InAmount)
+{
+}
---
+++ ProjectTwo/src/Core/Actor.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include <string>
+
+#include "Core/Object.h"
+#include "Core/Utilities/Vector2i.h"
+#include "Utilities/Transform.h"
+
+class Renderer;
+class World;
+
+class Actor : public Object
+{
+public:
+    Actor(World* InOwningWorld);
+    virtual ~Actor();
+
+    void BeginPlayInternal();
+    void TickInternal(float DeltaTime);
+    virtual void BeginPlay();
+    virtual void Tick(float DeltaTime);
+
+    virtual void Render(Renderer& InRendererRef);
+
+    void SetActorLocation(const Vector2i InNewLocation);
+    Vector2i GetActorLocation() const { return m_Transform.GetPosition(); }
+
+    const World* GetWorld() const { return m_OwningWorld; }
+    World* GetWorld() { return m_OwningWorld; }
+
+    virtual void Destroy() override;
+
+    virtual void ApplyDamage(float InAmount);
+
+    std::string& GetSprite() { return m_Sprite; }
+
+private:
+    World* m_OwningWorld;
+    bool m_HasBeganPlay;
+    bool m_IsRenderable;
+
+    std::string m_Sprite = "*";
+    int m_OverrideColor = -1;
+
+    Transform m_Transform = Transform();
+};
