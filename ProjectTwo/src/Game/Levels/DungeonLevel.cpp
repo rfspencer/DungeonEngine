44ca69db4315e19d901aac2ca0c5fcdbc68b3733
Rich <rfspencer@users.noreply.github.com>
2024-07-27 19:17:50 -0700

    Dungeon level loads HUD and layout

--- ProjectTwo/src/Game/Levels/DungeonLevel.cpp
+++ ProjectTwo/src/Game/Levels/DungeonLevel.cpp
@@ -1,1 +1,39 @@
-﻿#include "DungeonLevel.h"
+﻿#include "Game/Levels/DungeonLevel.h"
+
+#include <memory>
+
+#include "Application.h"
+#include "MainMenuLevel.h"
+#include "Player.h"
+#include "Game/Widgets/GameplayHUD.h"
+
+DungeonLevel::DungeonLevel(Application* InOwningApp)
+    : World(InOwningApp)
+{
+    m_GameplayHUD = SpawnHUD<GameplayHUD>();
+    // m_Player = SpawnActor<Player>();
+}
+
+void DungeonLevel::BeginPlay()
+{
+
+}
+
+void DungeonLevel::Tick(float DeltaTime)
+{
+}
+
+void DungeonLevel::RemoveListenerForInput() const
+{
+}
+
+void DungeonLevel::HandleInput(int InKeyPressed)
+{
+}
+
+void DungeonLevel::QuitGame()
+{
+    GetApplication()->GetRendererRef().ClearConsoleScreen();
+    RemoveListenerForInput();
+    GetApplication()->LoadWorld<MainMenuLevel>();
+}
--- ProjectTwo/src/Game/Levels/DungeonLevel.h
+++ ProjectTwo/src/Game/Levels/DungeonLevel.h
@@ -1,7 +1,26 @@
 ﻿#pragma once
 
-class DungeonLevel
+#include "World.h"
+
+class Player;
+class GameplayHUD;
+
+class DungeonLevel : public World
 {
 public:
+    DungeonLevel(Application* InOwningApp);
+    void BeginPlay() override;
+    void Tick(float DeltaTime) override;
+
+    void RemoveListenerForInput() const;
     
+private:
+    void HandleInput(int InKeyPressed);
+    
+    void QuitGame();
+
+    WeakPtr<Player> m_Player;    
+    WeakPtr<GameplayHUD> m_GameplayHUD;
+    
+    std::function<void(int)> m_InputEvent;
 };
