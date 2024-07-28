ff855005c686da33b3273bd8df3d7e4b1be59c68
Rich <rfspencer@users.noreply.github.com>
2024-07-27 19:16:44 -0700

    GameplayHUD loads layout file

---
+++ ProjectTwo/src/Game/Widgets/GameplayHUD.cpp
@@ -0,0 +1,27 @@
+﻿#include "GameplayHUD.h"
+
+#include "Utilities/FileHandler.h"
+
+const char* DATA_GAMEPLAY_LAYOUT_PATH = "src/Game/Data/GameplayHUD.layout";
+
+GameplayHUD::GameplayHUD()
+{
+    Init();
+}
+
+void GameplayHUD::Init()
+{
+    // TODO: Ideally this should be handled in an Asset Manager class and cached
+    if (FileHandler::DoesFileExist(DATA_GAMEPLAY_LAYOUT_PATH))
+    {
+        std::string TempString = FileHandler::ReadFile(DATA_GAMEPLAY_LAYOUT_PATH);
+        m_GameplayHUDBackground = FileHandler::StringToTextWidget(TempString);
+    }
+    m_GameplayHUDBackground.SetWidgetPosition(0, 0);
+}
+
+void GameplayHUD::Render(Renderer& InRendererRef)
+{
+    m_GameplayHUDBackground.RenderInternal(InRendererRef, true);
+}
+
---
+++ ProjectTwo/src/Game/Widgets/GameplayHUD.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "Widgets/HUD.h"
+#include "Widgets/TextWidget.h"
+
+class GameplayHUD : public HUD
+{
+public:
+    GameplayHUD();
+    
+    void Render(Renderer& InRendererRef) override;
+
+private:
+    void Init() override;
+
+    TextWidget m_GameplayHUDBackground;
+};
