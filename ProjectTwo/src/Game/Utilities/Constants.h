eac5220693fe4d18a4cece7b14c53b949c440fbe
Rich <rfspencer@users.noreply.github.com>
2024-07-27 20:04:38 -0700

    Color constants

--- ProjectTwo/src/Game/Utilities/Constants.h
+++ ProjectTwo/src/Game/Utilities/Constants.h
@@ -5,3 +5,13 @@
 const std::wstring GAME_NAME = L"Dungeon Crawler";
 constexpr size_t WINDOW_WIDTH = 120;
 constexpr size_t WINDOW_HEIGHT = 30;
+
+// 1 Blue
+// 2 Green
+// 3 Teal
+// 4 Red
+// 5 Violet
+// 6 Mustard
+// 7 White
+// 8 Grey
+// 9 Light Blue
