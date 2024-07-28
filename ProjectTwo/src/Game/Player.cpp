ba44957fae4fe8402f715791ac5d0ef587f1227e
Rich <rfspencer@users.noreply.github.com>
2024-07-27 20:04:08 -0700

    Adds Player class and implements movement

---
+++ ProjectTwo/src/Game/Player.cpp
@@ -0,0 +1,67 @@
+﻿#include "Player.h"
+
+#include <iostream>
+
+#include "Input.h"
+
+Player::Player(World* InOwningWorld)
+    : Actor(InOwningWorld), m_MoveSpeed(1)
+{
+    Init();
+    SetOverrideColor(m_PlayerSettings.Color);
+}
+
+void Player::Init()
+{
+    SetSprite(m_PlayerSettings.Sprite);
+    
+    m_InputEvent = std::bind(&Player::HandleInput, this, std::placeholders::_1);
+    Input::AddListener(m_InputEvent);
+}
+
+void Player::Update(float DeltaTime)
+{
+}
+
+void Player::RemoveListenerForInput()
+{
+    Input::RemoveListener(m_InputEvent);
+}
+
+bool Player::CanMove()
+{
+    return true;
+}
+
+void Player::Move(const Vector2i InOffset)
+{
+    if (CanMove())
+    {
+        const Vector2i CurrentLocation = GetActorLocation();
+        SetActorLocation(CurrentLocation + InOffset);
+    }
+}
+
+void Player::HandleInput(int InKeyPressed)
+{
+    if (InKeyPressed == tolower(PlayerSettings::InputKey::Up))
+    {
+        const Vector2i Direction{0, -1};
+        Move(Direction);
+    }
+    if (InKeyPressed == tolower(PlayerSettings::InputKey::Down))
+    {
+        const Vector2i Direction{0, 1};
+        Move(Direction);
+    }
+    if (InKeyPressed == tolower(PlayerSettings::InputKey::Left))
+    {
+        const Vector2i Direction{-1, 0};
+        Move(Direction);
+    }
+    if (InKeyPressed == tolower(PlayerSettings::InputKey::Right))
+    {
+        const Vector2i Direction{1, 0};
+        Move(Direction);
+    }
+}
---
+++ ProjectTwo/src/Game/Player.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "Actor.h"
+
+#include <functional>
+
+struct PlayerSettings
+{
+    enum InputKey
+    {
+        Up = 'w',
+        Down = 's',
+        Left = 'a',
+        Right = 'd',
+        Interact = 'e',
+        Endgame = 0
+    };
+
+    std::string Sprite = "@";
+    unsigned int MovementSpeed = 1;
+    int Color = 2;
+};
+
+class Player : public Actor
+{
+public:
+    Player(World* InOwningWorld);
+    void Init();
+    void Update(float DeltaTime);
+    void RemoveListenerForInput();
+    void SetMoveSpeed(int InSpeed) { m_MoveSpeed = InSpeed; }
+
+    bool CanMove();
+    void Move(const Vector2i InOffset);
+
+private:
+    void HandleInput(int InKeyPressed);
+
+    PlayerSettings m_PlayerSettings;
+    unsigned int m_MoveSpeed;
+
+    std::function<void(int)> m_InputEvent;
+};
