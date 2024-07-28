d543a5d77d8c5c73193279117a34e739625e3b50
Rich <rfspencer@users.noreply.github.com>
2024-07-28 13:23:04 -0700

    Adds Map class

---
+++ ProjectTwo/src/Core/Map.cpp
@@ -0,0 +1,19 @@
+﻿#include "Map.h"
+
+#include "Utilities/FileHandler.h"
+
+Map::Map(World* InOwningWorld, const std::string& InPath)
+    : Actor(InOwningWorld)
+{
+    Init(InPath);
+}
+
+void Map::Init(const std::string& InPath)
+{
+    // Initialize Map Widget
+    if (FileHandler::DoesFileExist(InPath.c_str()))
+    {
+        std::string TempString = FileHandler::ReadFile(InPath.c_str());
+        m_MapLayout = FileHandler::StringToMap(TempString);
+    }
+}
---
+++ ProjectTwo/src/Core/Map.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <string>
+
+#include "Application.h"
+#include "Core/Actor.h"
+#include "Core/Widgets/TextWidget.h"
+
+
+class Map : public Actor
+{
+public:
+    Map(World* InOwningWorld, const std::string& InPath = "");
+
+    void Init(const std::string& InPath);
+
+    // Overideing Render from Actor to draw as UI instead of an Actor
+    // void Render(Renderer& InRendererRef) override;
+
+    std::array<char, WINDOW_WIDTH * WINDOW_HEIGHT>& GetMap() { return m_MapLayout; }
+
+private:
+
+    // The actual map data to test against
+    std::array<char, WINDOW_WIDTH * WINDOW_HEIGHT> m_MapLayout;
+};
