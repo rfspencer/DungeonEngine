5cdc0b203610ad031f917cffc43c4b181a96f138
Rich <rfspencer@users.noreply.github.com>
2024-07-29 12:23:33 -0700

    DungeonLevel spawns in different Actor types

--- ProjectTwo/src/Game/Levels/DungeonLevel.cpp
+++ ProjectTwo/src/Game/Levels/DungeonLevel.cpp
@@ -39,6 +39,26 @@ void DungeonLevel::BeginPlay()
     WeakPtr<PickUp> GoldPickup1 = SpawnActor<PickUp>(Gold);
     GoldPickup1.lock()->SetActorLocation({62, 7});
     m_Map.lock()->AddActorToMap(GoldPickup1.lock().get());
+    
+    WeakPtr<PickUp> GoldPickup2 = SpawnActor<PickUp>(Gold);
+    GoldPickup2.lock()->SetActorLocation({36, 14});
+    m_Map.lock()->AddActorToMap(GoldPickup2.lock().get());
+    
+    WeakPtr<PickUp> GoldPickUp3 = SpawnActor<PickUp>(Gold);
+    GoldPickUp3.lock()->SetActorLocation({86, 10});
+    m_Map.lock()->AddActorToMap(GoldPickUp3.lock().get());
+    
+    WeakPtr<PickUp> Potion1 = SpawnActor<PickUp>(Health);
+    Potion1.lock()->SetActorLocation({20, 8});
+    m_Map.lock()->AddActorToMap(Potion1.lock().get());
+    
+    WeakPtr<PickUp> Potion2 = SpawnActor<PickUp>(Health);
+    Potion2.lock()->SetActorLocation({60, 15});
+    m_Map.lock()->AddActorToMap(Potion2.lock().get());
+    
+    WeakPtr<PickUp> Potion3 = SpawnActor<PickUp>(MaxHealth);
+    Potion3.lock()->SetActorLocation({80, 7});
+    m_Map.lock()->AddActorToMap(Potion3.lock().get());
 }
 
 void DungeonLevel::Tick(float DeltaTime)
