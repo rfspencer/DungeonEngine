8257891cd35987442b8a7445cf56556380f57255
Rich <rfspencer@users.noreply.github.com>
2024-07-29 11:48:57 -0700

    DungeonLevel can spawn in Actors at runtime

--- ProjectTwo/src/Game/Levels/DungeonLevel.cpp
+++ ProjectTwo/src/Game/Levels/DungeonLevel.cpp
@@ -2,6 +2,8 @@
 
 #include <memory>
 
+#include "Input.h"
+#include "Actors/PickUp.h"
 #include "Core/Application.h"
 #include "Core/Map.h"
 #include "Game/Levels/MainMenuLevel.h"
@@ -18,22 +20,47 @@ DungeonLevel::DungeonLevel(Application* InOwningApp)
 
 void DungeonLevel::BeginPlay()
 {
+    // Level listening for Quit game
+    m_InputEvent = std::bind(&DungeonLevel::HandleInput, this, std::placeholders::_1);
+    Input::AddListener(m_InputEvent);
+
+    // Begin spawning actors and HUD
     m_Map = SpawnActor<Map>(DATA_DUNGEON_MAP_PATH);
     m_GameplayHUD = SpawnHUD<GameplayHUD>();
 
+    // Spawn Player
     m_Player = PlayerManager::Get().CreateNewPlayer(this);
     // Set player start location
     m_Player.lock()->SetActorLocation({14, 22});
 
     m_GameplayHUD.lock()->BindDelegates();
+
+    // Adding pickups to the level
+    WeakPtr<PickUp> GoldPickup1 = SpawnActor<PickUp>(Gold);
+    GoldPickup1.lock()->SetActorLocation({62, 7});
+    m_Map.lock()->AddActorToMap(GoldPickup1.lock().get());
 }
 
 void DungeonLevel::Tick(float DeltaTime)
 {
 }
 
+void DungeonLevel::RemoveListenerForInput() const
+{
+    Input::RemoveListener(m_InputEvent);
+}
+
 void DungeonLevel::QuitGame()
 {
+    PlayerManager::Get().ResetPlayer();
     GetApplication()->GetRendererRef().ClearConsoleScreen();
     GetApplication()->LoadWorld<MainMenuLevel>();
+}
+
+void DungeonLevel::HandleInput(int InKeyPressed)
+{
+    if (InKeyPressed == '0')
+    {
+        QuitGame();
+    }
 }
--- ProjectTwo/src/Game/Levels/DungeonLevel.h
+++ ProjectTwo/src/Game/Levels/DungeonLevel.h
@@ -13,12 +13,16 @@ public:
     void BeginPlay() override;
     void Tick(float DeltaTime) override;
 
+    void RemoveListenerForInput() const;
+
     WeakPtr<Map> GetMap() const { return m_Map; }
     WeakPtr<Player> GetPlayer() override { return m_Player; }
 
     void QuitGame();
     
-private:    
+private:
+    void HandleInput(int InKeyPressed);
+    
     WeakPtr<Player> m_Player;    
     WeakPtr<GameplayHUD> m_GameplayHUD;
     WeakPtr<Map> m_Map;
