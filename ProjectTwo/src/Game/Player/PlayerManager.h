342131c754895786dc98ecc634ef70f3bba31d7d
Rich <rfspencer@users.noreply.github.com>
2024-07-29 07:03:24 -0700

    Adds PlayerManager class to created and manager Player instances

---
+++ ProjectTwo/src/Game/Player/PlayerManager.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "Player.h"
+
+class PlayerManager
+{
+public:
+    std::weak_ptr<Player> CreateNewPlayer(World* InOwningWorld);
+
+    std::weak_ptr<Player> GetPlayer();
+    std::weak_ptr<Player> GetPlayer() const;
+
+    static PlayerManager& Get();
+
+    void ResetPlayer();
+
+protected:
+    PlayerManager() = default;
+
+private:
+    // Making this a List to later extend to multiple players 
+    List<SharedPtr<Player>> m_Players;
+
+    static UniquePtr<PlayerManager> m_PlayerManager;
+};
