89ad2f705d165333ad272e972735c67cde167af8
Rich <rfspencer@users.noreply.github.com>
2024-07-29 11:47:57 -0700

    Adds PickUp class

---
+++ ProjectTwo/src/Game/Actors/PickUp.cpp
@@ -0,0 +1,39 @@
+﻿#include "PickUp.h"
+
+#include "World.h"
+#include "Player/Player.h"
+
+PickUp::PickUp(World* InOwningWorld, PickUpType InType)
+    : Actor(InOwningWorld)
+{
+    switch (InType)
+    {
+    case Gold:
+        {
+            // m_GiveFunction = std::bind(&PickUp::GiveGold, this, std::placeholders::_1);
+            SetSprite("$");
+            SetOverrideColor(6);
+            m_InteractionPrompt = "You found some Gold!";
+            break;    
+        }
+        
+    }
+}
+
+void PickUp::OnInteract()
+{
+    m_GiveFunction(GetWorld()->GetPlayer());
+}
+
+void PickUp::GiveGold(WeakPtr<Player> InPlayer)
+{
+    InPlayer.lock()->AddToGold();
+}
+
+void PickUp::GiveHP(WeakPtr<Player> InPlayer)
+{
+}
+
+void PickUp::GiveMaxHP(WeakPtr<Player> InPlayer)
+{
+}
---
+++ ProjectTwo/src/Game/Actors/PickUp.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "Actor.h"
+#include "Game/Interface/Interact.h"
+
+class Player;
+
+enum PickUpType
+{
+    Health,
+    MaxHealth,
+    Gold
+};
+
+// using GiveFunction = std::function<void(WeakPtr<Player>)>;
+typedef std::function<void(WeakPtr<Player>)> GiveFunction;
+
+class PickUp : public Actor, public Interact
+{
+public:
+    PickUp(World* InOwningWorld, PickUpType InType);
+
+    void OnInteract();
+
+    std::string GetInteractionPrompt() { return m_InteractionPrompt; }
+    void SetInteractionPrompt(const std::string& InString) { m_InteractionPrompt = InString; }
+
+    int GetPickUpAmount() const { return m_PickUpAmount; }
+    void SetPickUpAmount(const int InAmount) { m_PickUpAmount = InAmount; } // TODO: Should check if not negative before setting
+    
+    void GiveGold(WeakPtr<Player> InPlayer);
+    void GiveHP(WeakPtr<Player> InPlayer);
+    void GiveMaxHP(WeakPtr<Player> InPlayer);
+private:
+
+    GiveFunction m_GiveFunction;
+    
+    std::string m_InteractionPrompt;
+    int m_PickUpAmount;
+};
