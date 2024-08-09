ca9d5c772f98568557100014dd55d4cc8fd4d52a
Rich <rfspencer@users.noreply.github.com>
2024-08-08 19:44:50 -0700

    Adds Premake as build system

---
+++ premake5.lua
@@ -0,0 +1,128 @@
+workspace "DungeonEngine"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+-- IncludeDir["GLFW"] = "Engine/thirdparty/GLFW/include"
+
+group "Dependencies"
+	-- include "Engine/thirdparty/GLFW"
+group ""
+
+project "Engine"
+	location "Engine"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	-- pchheader "depch.h"
+	-- pchsource "Engine/src/depch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src"
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	links
+	{
+		-- "GLFW",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"DE_PLATFORM_WINDOWS",
+			"DE_BUILD_DLL"
+		}
+
+	filter "configurations:Debug"
+		defines "DE_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "DE_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "DE_DIST"
+		runtime "Release"
+		optimize "on"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Engine/src"
+	}
+
+	links
+	{
+		"Engine"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"DE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "DE_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "DE_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "DE_DIST"
+		runtime "Release"
+		optimize "on"
